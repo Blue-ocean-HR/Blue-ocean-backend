3cebc74456e9d3de4e1477533d5285f2b489e84e
Andrew <drew.311.a@gmail.com>
2022-11-22 01:58:00 -0500

    Added query for adding an ingredient to pantry
    --Must pass in an ingredient name, date, category and registered user as query params

--- db/models/pantry/addItem.js
+++ db/models/pantry/addItem.js
@@ -1,7 +1,68 @@
 const connectionPool = require('../../utils/connect.js')
 
 const addItem = (req, res, next) => {
-  res.send('addItem')
+
+  let name = req.query.name;
+  let date = req.query.date;
+  let category = req.query.category;
+  let email = req.query.email;
+
+  const userExists = {
+    text: `select exists (select 1 from users where email = ${email})`
+  }
+
+  if (name && date && category && email) {
+    console.log('yes')
+    connectionPool.query(userExists)
+    .then((data) => {
+      if (data.rows[0].exists === false) {
+        res.send('Please make sure you send a registered user')
+      } else {
+        connectionPool.
+        query(
+          `insert into pantry (
+            user_id, pantry_ingredient, expiry_date,
+            category
+          )
+          values
+            (
+              (
+                select
+                  id
+                from
+                  users
+                where
+                  email = ${email}
+              ),
+              ${name},
+              ${date},
+              ${category}
+            )
+          `
+        )
+        .then((data) => {
+          res.sendStatus(200)
+        })
+        .catch((err) => {
+          console.log(err)
+          res.sendStatus(500)
+        })
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(500)
+    })
+
+  } else {
+    res.send('Please make sure you are sending an ingredient name, a date, a category and a registered email')
+  }
+
+  // res.send('addItem')
 }
 
 module.exports = addItem
+// Create: Takes in a name, date, category and email
+// Read: Takes in a email
+// Update: Takes in a IngredientName, ExpiryDate, pantryId
+// Destroy: Removes an Ingredient, Takes in an pantryId
