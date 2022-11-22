9f2ae5b83d55668ac26183b1c1140a8c51fb6386
Diana Chong <dchong0123@gmail.com>
2022-11-22 15:10:06 -0500

    added db query to remove pantry id given pantryid

--- db/models/pantry/deleteItem.js
+++ db/models/pantry/deleteItem.js
@@ -1,7 +1,11 @@
 const connectionPool = require('../../utils/connect.js')
+const {userExistsStatement} = require('../statements/addPantryItem.js')
 
 const deleteItem = (req, res, next) => {
-  res.send('deleteItem')
+  let id=req.query.pantryId
+  connectionPool.query(`delete from pantry where id=${id}`)
+    .then((response) => res.send('deleteItem'))
+    .catch((error) => console.log('ERROR: ', error));
 }
 
 module.exports = deleteItem
