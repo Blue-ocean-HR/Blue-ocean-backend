9486ed1af7ddec2af44f69117b1d49029657a916
Maxwell Philip <max.philip1@gmail.com>
2022-11-22 16:38:00 -0500

    ignore this again

--- db/finalSchemaBO.sql
+++ db/finalSchemaBO.sql
@@ -47,7 +47,7 @@ CREATE TABLE recipes(
 
 COPY recipes (id, title, recipes_ingredients, directions, link)
 
-FROM '/Users/dianachong/HR/BlueOcean/recipes1.csv'
+FROM '/Users/angelacarrasco/Documents/blue-ocean-data/recipes1.csv'
 
 DELIMITER ',' CSV QUOTE '"';
 
@@ -64,7 +64,7 @@ CREATE TABLE recipe_ingredients(
 
 COPY recipe_ingredients (ingredients_name, recipes_id)
 
-FROM '/Users/dianachong/HR/BlueOcean/bo-ingredients.csv'
+FROM '/Users/angelacarrasco/Documents/blue-ocean-data/bo-ingredients.csv'
 
 DELIMITER ',' CSV HEADER QUOTE '"';
 
